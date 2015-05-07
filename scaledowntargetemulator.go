3e7bb18818059ebda1eac9d08e8910abcc949569
Marko Luksa <marko.luksa@gmail.com>
2015-05-07 09:01:12 +0200

    Added handler for /health

--- scaledowntargetemulator.go
+++ scaledowntargetemulator.go
@@ -8,6 +8,17 @@ import (
 	"log"
 )
 
+var health string = "ok"
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	newHealth := r.Form.Get("value")
+	if newHealth != "" {
+		health = newHealth
+	}
+	w.Write([]byte(health))
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
 }
@@ -26,5 +37,6 @@ func addSignalTrap() {
 func main() {
 	addSignalTrap()
 	http.HandleFunc("/", handler)
+	http.HandleFunc("/health", healthHandler)
 	http.ListenAndServe(":8080", nil)
 }
